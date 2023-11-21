77dc12c8cd18323880f104749e5acc638e5cb1b4
Julius Volland <julius.volland@capgemini.com>
2023-11-21 15:06:31 +0100

    DBP-363: Get Ionos Servers (#4)
    * DBP-363: Add GetServers method to Provider
    
    * DBP-363: Expand environment variables in config yaml

--- providers/Ionos.go
+++ providers/Ionos.go
@@ -1,40 +1,90 @@
 package providers
 
 import (
+	"context"
 	"fmt"
+	"regexp"
 	s "scaler/shared"
+
+	ic "github.com/ionos-cloud/sdk-go/v6"
 )
 
 type ProviderConfig struct {
-	Username     string
-	Password     string
+	Username     s.StringFromEnv `yaml:"username"`
+	Password     s.StringFromEnv `yaml:"password"`
 	ServerSource *s.ServerSource `yaml:"server_source"`
-	// PostgresSource *PostgresSource `yaml:"postgres_source"`
 }
 
 type Ionos struct {
-	IonosConfig ProviderConfig `yaml:"ionos_config"`
+	Config ProviderConfig `yaml:"ionos_config"`
+	Api    ic.APIClient   `yaml:"-"`
 }
 
-func (i Ionos) Get_login_id() string {
-	return i.IonosConfig.Username
+func (i *Ionos) Init() error {
+	i.Api = *ic.NewAPIClient(ic.NewConfiguration(
+		string(i.Config.Username),
+		string(i.Config.Password),
+		"",
+		""))
+	return nil
 }
 
-func (i Ionos) Get_login_secret() string {
-	return i.IonosConfig.Password
+func (i Ionos) GetServers(depth int) ([]s.Server, error) {
+	var servers []s.Server
+
+	if i.Config.ServerSource.Static != nil {
+		getServersStatic(&servers, i)
+	} else if i.Config.ServerSource.Dynamic != nil {
+		getServersDynamic(&servers, i, depth)
+	}
+	return servers, nil
 }
 
-func (p Ionos) Validate() error {
-	if p.IonosConfig.Username == "" {
+func getServersStatic(servers *[]s.Server, i Ionos) {
+	for _, serverSource := range *i.Config.ServerSource.Static {
+		dcServer, _, err := i.Api.ServersApi.DatacentersServersFindById(context.Background(), serverSource.DatacenterId, serverSource.ServerId).Execute()
+		if err != nil {
+			fmt.Printf("error while getting servers: %s", err)
+		}
+		*servers = append(*servers, s.Server{
+			DatacenterId: serverSource.DatacenterId,
+			ServerId:     *dcServer.Id,
+			ServerCpu:    float64(*dcServer.Properties.Cores),
+			ServerRam:    float64(*dcServer.Properties.Ram),
+		})
+	}
+}
+
+func getServersDynamic(servers *[]s.Server, i Ionos, depth int) {
+	for _, datacenterId := range i.Config.ServerSource.Dynamic.DatacenterIds {
+		dcServers, _, err := i.Api.ServersApi.DatacentersServersGet(context.Background(), datacenterId).Depth(int32(depth)).Execute()
+		if err != nil {
+			fmt.Printf("error while getting servers: %s", err)
+		}
+		for _, dcServer := range *dcServers.Items {
+			if match, _ := regexp.MatchString(i.Config.ServerSource.Dynamic.ServerNameRegex, *dcServer.Properties.Name); match {
+				*servers = append(*servers, s.Server{
+					DatacenterId: datacenterId,
+					ServerId:     *dcServer.Id,
+					ServerCpu:    float64(*dcServer.Properties.Cores),
+					ServerRam:    float64(*dcServer.Properties.Ram),
+				})
+			}
+		}
+	}
+}
+
+func (i Ionos) Validate() error {
+	if i.Config.Username == "" {
 		return fmt.Errorf("username is empty")
 	}
-	if p.IonosConfig.Password == "" {
+	if i.Config.Password == "" {
 		return fmt.Errorf("password is empty")
 	}
-	if p.IonosConfig.ServerSource == nil {
+	if i.Config.ServerSource == nil {
 		return fmt.Errorf("server_source is nil")
 	} else {
-		if err := p.IonosConfig.ServerSource.Validate(); err != nil {
+		if err := i.Config.ServerSource.Validate(); err != nil {
 			return err
 		}
 	}
---
+++ shared/loader_test.go
@@ -0,0 +1,51 @@
+package shared
+
+import (
+	"os"
+	"testing"
+)
+
+type TestStruct struct {
+	Test1 StringFromEnv `yaml:"test1"`
+	Test2 StringFromEnv `yaml:"test2"`
+	Test3 StringFromEnv `yaml:"test3"`
+}
+
+func (t TestStruct) Validate() error {
+	return nil
+}
+
+func TestLoadEnv(t *testing.T) {
+	os.Setenv("TEST_ENV_1", "value1")
+	defer os.Unsetenv("TEST_ENV_1")
+
+	testConfig, err := OpenConfig("test_files/env.yaml")
+	if err != nil {
+		t.Fatal(err)
+	}
+	testStruct, err := LoadConfig[TestStruct](testConfig)
+	if err != nil {
+		t.Errorf("unexpected error unmarshalling the config yaml: %s", err)
+	}
+
+	if testStruct.Test1 != "value1" {
+		t.Errorf("expected Test1 to be value1, got %s", testStruct.Test1)
+	}
+	if testStruct.Test2 != "$Test_env_2" {
+		t.Errorf("expected Test2 to be $Test_env_2, got %s", testStruct.Test2)
+	}
+	if testStruct.Test3 != "not_an_env_var" {
+		t.Errorf("expected Test3 to be not_an_env_var, got %s", testStruct.Test3)
+	}
+}
+
+func TestLoadUnsetEnv(t *testing.T) {
+	testConfig, err := OpenConfig("test_files/env.yaml")
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = LoadConfig[TestStruct](testConfig)
+	if err == nil {
+		t.Error("expected error: environment variable TEST_ENV_1 not set")
+	}
+}
